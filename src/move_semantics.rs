9b45b73bd550eaf29bdc8ccf69b2e6f49d2b54ed
Vaibhav <thevaibhavdixit@gmail.com>
2025-06-02 07:19:49 +0530

    feat: Pin for self referential structs

--- src/move_semantics.rs
+++ src/move_semantics.rs
@@ -107,4 +107,97 @@ mod test {
         // that Pin provides.
         // let new_tracker = tracker;
     }
+
+    #[test]
+    fn box_moves() {
+        #[derive(Debug, Default)]
+        struct Data<'a> {
+            value: String,
+            value_str: Option<&'a str>,
+        }
+        let value = "hello".to_string();
+        let mut data = Data {
+            value,
+            value_str: None,
+        };
+        data.value_str = Some(&data.value);
+
+        let boxed_ref = Box::new(&data);
+        assert_eq!(boxed_ref.value_str, Some("hello"));
+
+        let new_boxed_ref = boxed_ref;
+        assert_eq!(new_boxed_ref.value_str, Some("hello"));
+
+        // can not borrow data as mutable since it is also borrowed as immutable
+        // std::mem::swap(&mut data, &mut Data::default());
+    }
+
+    #[test]
+    fn pinned_self_referential_struct() {
+        // use std::marker::PhantomPinned;
+        use std::pin::Pin;
+        use std::ptr::NonNull;
+        /// This is a self-referential struct because `self.slice` points into `self.data`.
+        struct Unmovable {
+            /// Backing buffer.
+            data: [u8; 64],
+            /// Points at `self.data` which we know is itself non-null. Raw pointer because we can't do
+            /// this with a normal reference.
+            slice: NonNull<[u8]>,
+            // _pin: PhantomPinned,
+        }
+
+        impl Unmovable {
+            /// Creates a new `Unmovable`.
+            ///
+            /// To ensure the data doesn't move we place it on the heap behind a pinning Box.
+            /// Note that the data is pinned, but the `Pin<Box<Self>>` which is pinning it can
+            /// itself still be moved. This is important because it means we can return the pinning
+            /// pointer from the function, which is itself a kind of move!
+            fn new(i: u8) -> Pin<Box<Self>> {
+                let res = Unmovable {
+                    data: [i; 64],
+                    // We only create the pointer once the data is in place
+                    // otherwise it will have already moved before we even started.
+                    slice: NonNull::from(&[]),
+                    // _pin: PhantomPinned,
+                };
+                // First we put the data in a box, which will be its final resting place
+                let mut boxed = Box::new(res);
+
+                // Then we make the slice field point to the proper part of that boxed data.
+                // From now on we need to make sure we don't move the boxed data.
+                boxed.slice = NonNull::from(&boxed.data);
+
+                // To do that, we pin the data in place by pointing to it with a pinning
+                // (`Pin`-wrapped) pointer.
+                //
+                // `Box::into_pin` makes existing `Box` pin the data in-place without moving it,
+                // so we can safely do this now *after* inserting the slice pointer above, but we have
+                // to take care that we haven't performed any other semantic moves of `res` in between.
+                let pin = Box::into_pin(boxed);
+
+                // Now we can return the pinned (through a pinning Box) data
+                pin
+            }
+        }
+
+        let mut unmovable: Pin<Box<Unmovable>> = Unmovable::new(1);
+
+        // The inner pointee `Unmovable` struct will now never be allowed to move.
+        // Meanwhile, we are free to move the pointer around.
+        // let mut still_unmoved = unmovable;
+        // assert_eq!(still_unmoved.slice, NonNull::from(&still_unmoved.data));
+
+        let mut new_unmovable = Unmovable::new(2);
+        let prev_slice = new_unmovable.slice;
+
+        std::mem::swap(&mut *new_unmovable, &mut *unmovable);
+        let current_slice = new_unmovable.slice;
+
+        // without PhantomPinned, this would panic because the address of `slice` would change
+        // and this is how pinned is useful if the struct was PhantomPinned it would be !Unpin and
+        // hence we could not have deref_mut which changes the inner values
+        assert_eq!(prev_slice, current_slice);
+    }
 }
