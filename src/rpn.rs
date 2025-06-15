5845e67b556b5a29b9477aec05dea7d851210eec
Vaibhav <thevaibhavdixit@gmail.com>
2025-06-15 16:11:07 +0530

    feat: expand scope to multiple digit numbers

--- src/rpn.rs
+++ src/rpn.rs
@@ -1,14 +1,11 @@
 struct Rpn {
-    source: Vec<char>,
+    source: Vec<String>,
     stack: Vec<i16>,
 }
 
 impl Rpn {
     fn new(source: String) -> Self {
-        let source = source
-            .chars()
-            .filter(|c| !c.is_whitespace())
-            .collect::<Vec<_>>();
+        let source = source.split_whitespace().map(String::from).collect();
 
         Rpn {
             source,
@@ -17,7 +14,30 @@ impl Rpn {
     }
 
     fn eval(&mut self) -> i32 {
-        0
+        for c in &self.source {
+            if c.chars().all(char::is_numeric) {
+                let num: i16 = c.parse().expect("Failed to parse number");
+                self.stack.push(num);
+            } else {
+                let b = self.stack.pop().expect("Stack underflow");
+                let a = self.stack.pop().expect("Stack underflow");
+
+                match c.chars().next().expect("Empty operator") {
+                    '+' => self.stack.push(a + b),
+                    '-' => self.stack.push(a - b),
+                    '*' => self.stack.push(a * b),
+                    '/' => {
+                        if b == 0 {
+                            panic!("Division by zero");
+                        }
+                        self.stack.push(a / b);
+                    }
+                    _ => panic!("Unknown operator: {}", c),
+                }
+            }
+        }
+
+        self.stack.pop().expect("No result on stack") as i32
     }
 }
 
@@ -28,9 +48,28 @@ mod tests {
     #[test]
     fn trivial_case() {
         let input = "0";
-
         let mut rpn = Rpn::new(input.to_string());
-
         assert_eq!(rpn.eval(), 0);
+    }
+
+    #[test]
+    fn simple_addition() {
+        let input = "1 2 +";
+        let mut rpn = Rpn::new(input.to_string());
+        assert_eq!(rpn.eval(), 3);
+    }
+
+    #[test]
+    fn complex_evaluation() {
+        let input = "3 4 + 2 * 7 /";
+        let mut rpn = Rpn::new(input.to_string());
+        assert_eq!(rpn.eval(), 2);
+    }
+
+    #[test]
+    fn multiple_digits() {
+        let input = "12 34 +";
+        let mut rpn = Rpn::new(input.to_string());
+        assert_eq!(rpn.eval(), 46);
     }
 }
